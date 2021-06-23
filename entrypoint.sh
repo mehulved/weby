99bd5aae8c5f455b80668c2c52d3fb310a4cd13b
Mehul Ved <mehul.ved@nexsales.com>
2021-06-23 16:33:32 +0530

    Run db create and db seed only when database is empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,25 +14,27 @@ then
     mkdir /weby/log/
 fi
 
-# Create database
-echo "Creating database..."
-bundle exec rake db:create
-echo "Database created."
+if bundle exec rake db:exists; then
+  # Run migrations
+  echo "Running migrations..."
+  bundle exec rake db:migrate
+  echo "Migrations completed."
+else
+  # Create database
+  echo "Creating database..."
+  bundle exec rake db:create
+  echo "Database created."
 
-# Load Schema
-echo "Loading database schema..."
-bundle exec rake db:schema:load RAILS_ENV=$RAILS_ENV DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-echo "Schema loaded."
+  # Run migrations
+  echo "Running migrations..."
+  bundle exec rails db:migrate
+  echo "Migrations completed."
 
-# Run migrations
-echo "Running migrations..."
-bin/rails db:migrate RAILS_ENV=$RAILS_ENV
-echo "Migrations completed."
-
-# Add seed data
-echo "Seeding database..."
-bundle exec bin/rails db:seed RAILS_ENV=$RAILS_ENV
-echo "Database seeded."
+  # Add seed data
+  echo "Seeding database..."
+  bundle exec bin/rails db:seed RAILS_ENV=$RAILS_ENV
+  echo "Database seeded."
+fi
 
 
 rails s -b 0.0.0.0 -p 3000
