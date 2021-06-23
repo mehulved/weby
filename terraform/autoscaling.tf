9ff2b1e809a250273e244153fef8211aedfc56d5
Mehul Ved <mehul.ved@nexsales.com>
2021-06-23 17:37:32 +0530

    We need to specify lower bound for scaling up

--- terraform/autoscaling.tf
+++ terraform/autoscaling.tf
@@ -38,7 +38,7 @@ resource "aws_appautoscaling_policy" "ecs_policy_up" {
     metric_aggregation_type = "Maximum"
 
     step_adjustment {
-      metric_interval_upper_bound = 0
+      metric_interval_lower_bound = 0
       scaling_adjustment          = 1
     }
   }
